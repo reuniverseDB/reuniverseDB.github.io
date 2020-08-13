fbc788d858eb8bbb5fbfc85fbf556c8720f11144
Elaeagnifolia <j.wong205914@gmail.com>
2020-08-13 14:40:06 -0500

    Updates

---
+++ precache-manifest.10470f8caf0113a8dd3eb151683502bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8cd3ad29b0c9783a85672a46f1d76fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a978006fdaf3f579e257",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a46576780bd6faf10103",
+    "url": "/static/js/2.c4b65d7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4b65d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a978006fdaf3f579e257",
+    "url": "/static/js/main.554cde34.chunk.js"
+  },
+  {
+    "revision": "3bd531d94225afb7a153",
+    "url": "/static/js/runtime-main.06da614f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
